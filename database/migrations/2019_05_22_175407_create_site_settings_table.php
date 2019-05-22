33799c9d49a68d861f4f8f34c4a0fa26cc0fbd64
MarufSharia <marufsharia@gmail.com>
2019-05-23 04:22:13 +0600

    adeed sitesetting model

---
+++ database/migrations/2019_05_22_175407_create_site_settings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSiteSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('site_settings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('site_title')->default('MarufSharia.me');
+            $table->string('footer_copyright_text')->default('&copy; MarufSharia '.date('Y')); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('site_settings');
+    }
+}
