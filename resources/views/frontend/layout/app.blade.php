551820697860d0384a5e104b36933047507e2059
Maruf Sharia <imran@dream71.com>
2019-05-22 10:59:11 +0600

    added theme and bootstraping

---
+++ resources/views/frontend/layout/app.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Maruf Sharia</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width" />
+    <link rel="shortcut icon" href="favicon.png" />
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+    <link href="{{asset('frontend/style/all.min.css')}}" rel="stylesheet" type="text/css" />
+    <script src="{{asset('frontend/js/jquery-1.9.1.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('frontend/js/modernizr-2.6.2.min.js')}}" type="text/javascript"></script>
+    <!--[if lt IE 9]><script src="js/html5.js"></script><![endif]-->
+    <!--[if IE]><style>.timeline li{display:block;-ms-filter: "progid: DXImageTransform.Microsoft.Alpha(Opacity=100)";filter: alpha(opacity=100);opacity: 1;}</style><![endif]-->
+</head>
+
+@yield('content')
+
+<div class="clear"></div>
+<footer class="section">
+    <div class="container">
+        <p>&copy;2019 marufsharia.me. All Rights Reserved.</p>
+        <ul>
+            <li>
+                <a target="_blank" class="facebook" href="http://www.facebook.com/"></a>
+            </li>
+            <li>
+                <a target="_blank" class="twitter" href="http://www.twitter.com/"></a>
+            </li>
+            <li>
+                <a target="_blank" class="google" href="https://plus.google.com/"></a>
+            </li>
+            <li>
+                <a target="_blank" class="linkedin" href="http://www.linkedin.com/"></a>
+            </li>
+            <li>
+                <a target="_blank" class="instagram" href="http://www.instagram.com/"></a>
+            </li>
+            <li>
+                <a target="_blank" class="youtube" href="https://www.youtube.com/"></a>
+            </li>
+            <li>
+                <a target="_blank" class="vimeo" href="http://www.vimeo.com/"></a>
+            </li>
+            <li>
+                <a target="_blank" class="tumblr" href="http://www.tumblr.com/"></a>
+            </li>
+            <li>
+                <a target="_blank" class="dribbble" href="http://www.dribbble.com/"></a>
+            </li>
+            <li>
+                <a target="_blank" class="behance" href="https://www.behance.net/"></a>
+            </li>
+            <li>
+                <a target="_blank" class="flickr" href="https://www.flickr.com/"></a>
+            </li>
+        </ul>
+    </div>
+</footer>
+<script src="https://maps.googleapis.com/maps/api/js?sensor=false" type="text/javascript"></script>
+<script src="{{asset('frontend/js/all.min.js')}}" type="text/javascript"></script>
+
+</body>
+
+</html>
